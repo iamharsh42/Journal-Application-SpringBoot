024f9dfe538ef0f58f5115311a0185e57142b610
iamharsh42 <tiwariharsh421@gmail.com>
2025-02-12 20:22:32 +0530

    created a REST API

--- src/main/java/com/harsh/journalApp/controllers/JournalEntryController.java
+++ src/main/java/com/harsh/journalApp/controllers/JournalEntryController.java
@@ -20,4 +20,19 @@ class JournalEntryController {
         journalEntries.put(myEntry.getId(), myEntry);
         return true;
     }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public JournalEntry deleteJournalById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("/id/{myId}")
+    public JournalEntry updateJournalById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry);
+    }
 }
